ac7a8a76b908b39716172f431ac25b9c5af6a1f0
Mesut Said OZTURK <msozturk12@gmail.com>
2023-05-12 19:11:41 +0300

    Qlub_Automation

--- qlupTestCase/payment_test.py
+++ qlupTestCase/payment_test.py
@@ -4,11 +4,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="module")
 def browser():
     # Setup: Create a WebDriver instance
+    chr_options = Options()
+    chr_options.add_experimental_option("detach", True)
+    global driver
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.implicitly_wait(10)
     driver.maximize_window()
@@ -20,41 +24,55 @@ def browser():
 
 def test_payment(browser):
     # Navigate to the payment page
-    browser.get("https://newapp-staging.qlub.cloud/qr/ae/dummy-checkout/90/_/_/1827c10c80?lang=en")
+    driver.get("https://newapp-staging.qlub.cloud/qr/ae/dummy-checkout/90/_/_/1827c10c80?lang=en")
 
     # Click on 'View menu'
-    browser.find_element(By.XPATH, "//a[normalize-space()='View menu']").click()
+    driver.find_element(By.XPATH, "//a[normalize-space()='View menu']").click()
 
     # Click on 'Pay the bill'
-    browser.find_element(By.XPATH, "//a[normalize-space()='Pay the bill']").click()
+    driver.find_element(By.XPATH, "//a[normalize-space()='Pay the bill']").click()
 
     # Click on 'Split bill'
-    browser.find_element(By.XPATH, "//span[normalize-space()='Split bill']").click()
+    driver.find_element(By.XPATH, "//span[normalize-space()='Split bill']").click()
 
     # Click on Pay a custom amount
-    browser.find_element(By.CSS_SELECTOR, "button[id='select-custom'] span[class='wrapper']").click()
+    driver.find_element(By.CSS_SELECTOR, "button[id='select-custom'] span[class='wrapper']").click()
 
     # Set amount 10 AED
-    browser.find_element(By.ID, "fullWidth").send_keys("10")
+    driver.find_element(By.ID, "fullWidth").send_keys("10")
 
     # Click confirm
-    browser.find_element(By.XPATH, "//span[normalize-space()='Confirm']").click()
+    driver.find_element(By.XPATH, "//span[normalize-space()='Confirm']").click()
 
     # Click on Tip option and choose one
     time.sleep(3)
-    browser.find_element(By.XPATH, "(//div[@class='MuiBox-root css-1tpu0u3'])[3]").click()
+    driver.find_element(By.XPATH, "(//div[@class='MuiBox-root css-1tpu0u3'])[3]").click()
 
-    # Enter information card
-    card_number = browser.find_element(By.ID, "checkout-frames-card-number")
+    ## Enter information card
+    # EnterCardNumber
+    driver.switch_to.frame("cardNumber")  # ıd
+    card_number = driver.find_element(By.ID, "checkout-frames-card-number")
     card_number.send_keys("4242 4242 4242 4242")
+    driver.switch_to.default_content()
+    time.sleep(2)
+    # EnterExpiryDate
+    driver.switch_to.frame("expiryDate")  # ıd
+    expiry_date = driver.find_element(By.ID, "checkout-frames-expiry-date")
+    expiry_date.send_keys("02/26")
+    driver.switch_to.default_content()
+    time.sleep(1)
+    # EnterCvv
+    driver.switch_to.frame("cvv") #ıd
+    cvv = driver.find_element(By.ID, "checkout-frames-cvv")
+    cvv.send_keys("100")
+    driver.switch_to.default_content()
+    time.sleep(3)
 
-    browser.find_element(By.ID, "checkout-frames-expiry-date").send_keys("02/26")
-    browser.find_element(By.ID, "checkout-frames-cvv").send_keys("100")
+    # Click on pay button
+    driver.find_element(By.XPATH, "//button[@id='checkout-action-btn']").click()
+    time.sleep(10)
 
-    # Click on pay
-    browser.find_element(By.XPATH, "//span[normalize-space()='Pay Now']").click()
-
-    #verify redirect to successful payment page
-    element= browser.find_element(By.XPATH,"(//p[@class='MuiTypography-root MuiTypography-body1 css-1dbb4wf'])[1]")
+    # verify redirect to successful payment page
+    element = browser.find_element(By.XPATH, "(//p[@class='MuiTypography-root MuiTypography-body1 css-1dbb4wf'])[1]")
     element_text = element.text
     assert element_text == "Payment was successful!"
